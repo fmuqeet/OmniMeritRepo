b3f0952bdf49fc4f72432fee88999b3f2a7dd5de
Faaz Muqeet <faazmuqeet24@gmail.com>
2016-12-19 22:51:44 +0530

    Registration and Login page

---
+++ OmniMerit-master/OmniMerit/Controllers/UserLoginController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OmniMerit.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        // GET: UserLogin
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities de = new MyDatabaseEntities())
+                {
+                    var v = de.Users.Where(model => model.Username.Equals(u.Username) && model.Password.Equals(u.Password)).FirstOrDefault();
+
+                    if(v != null)
+                    {
+                        Session["LoggedUserID"] = v.UserID.ToString();
+                        Session["LoggedUsername"] = v.FullName.ToString();
+                        //return RedirectToAction("AfterLogin");
+                        ViewBag.Message = "Login success";
+                        return View(u);
+                    }
+                }
+            }
+            return View(u);
+        }
+
+
+        public ActionResult AfterLogin()
+        {
+            if(Session["LoggedUserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            
+        }
+    }
+}
---
+++ OmniMerit-master/OmniMerit/Controllers/UserRegistrationController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OmniMerit.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        // GET: UserRegistration
+        public ActionResult Register()
+        {
+            List<SelectListItem> classList = new List<SelectListItem>();
+            classList.Add(new SelectListItem
+            {
+                Text = "8",
+                Value = "8th"
+            });
+            classList.Add(new SelectListItem
+            {
+                Text = "9",
+                Value = "9th"
+            });
+            classList.Add(new SelectListItem
+            {
+                Text = "10",
+                Value = "10th"
+            });
+            classList.Add(new SelectListItem
+            {
+                Text = "11",
+                Value = "11th"
+            });
+            classList.Add(new SelectListItem
+            {
+                Text = "12",
+                Value = "12th"
+            });
+            ViewBag.ClassList = classList;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities de = new MyDatabaseEntities())
+                {
+                    de.Users.Add(u);
+                    de.SaveChanges();
+                    ModelState.Clear();
+                    u = null;
+                    ViewBag.Message = "Successfully Registered with us. \nThanks!";
+                }
+            }
+            return View(u);
+        }
+    }
+}
